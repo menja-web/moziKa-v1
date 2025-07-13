3dc2f0252cccaacac4ca07f0d6a3e0d084304c29
menja-web <menjaniainarandriamaharitra@gmail.com>
2025-07-13 11:35:05 +0300

    Remove inline script, use external profil.js

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -2,7 +2,7 @@ function requireLogin(req, res, next) {
   if (!req.session || !req.session.userId) {
     return res.status(403).json({
       status: 'error',
-      message: 'Non connecté. Veuillez vous authentifier.'
+      message: 'Non connecté'
     });
   }
   next();
