8a68adad6c993c20e252f1927e23557d279cd85a
menja-web <menjaniainarandriamaharitra@gmail.com>
2025-07-15 16:21:24 +0300

    🐛 Fix middleware admin function

--- middleware/isAdmin.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  if (!req.user || !req.user.isAdmin) {
-    return res.status(403).json({ error: "Accès refusé : admin requis." });
-  }
-  next();
-};
---
+++ middlewares/isAdmin.js
@@ -0,0 +1,7 @@
+// middlewares/isAdmin.js
+module.exports = (req, res, next) => {
+  if (!req.user || !req.user.isAdmin) {
+    return res.status(403).json({ error: "Accès refusé : admin requis." });
+  }
+  next();
+};
